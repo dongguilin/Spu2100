16779bdd4ef03d0417b99598b0219621bfeaaaf2
guilin <guilin1_happy@163.com>
2014-07-09 16:49:05 +0800

    修改模拟通道配置

--- code/Spu2100/WebContent/js/spu/ad.js
+++ code/Spu2100/WebContent/js/spu/ad.js
@@ -12,16 +12,16 @@ var AD = function() {
 				"key" : "AD",
 				"type" : "multi"
 			}, function(data) {
+				$('#AD_key').empty();
 				jsondata = data;
 				var i = 0;
 				for ( var config in data) {
 					$('#AD_key').append($('<option>').text(config));
-
-					// 选中第一个
-					$("#AD_key").first().attr("selected", "selected");
 					// 填充表单
 					if (i == 0) {
-						fillForm(jsondata[config].items);
+						// 选中第一个
+						$("#AD_key").first().attr("selected", "selected");
+						fillForm(jsondata[config]);
 						i++;
 					}
 				}
@@ -31,16 +31,15 @@ var AD = function() {
 			// 下拉列表change事件
 			$('#AD_key').change(function() {
 				var key = $(this).val();
-				var items = jsondata[key].items;
+				var items = jsondata[key];
 				fillForm(items);
 			});
 
 			// 填充表单数据
 			function fillForm(items) {
 				for ( var i in items) {
-					var item = items[i];
-					var name = item.name;
-					var value = item.value;
+					var name = i;
+					var value = items[i];
 					var $select = $("#AD form").find(
 							"select[name=" + name + "]");
 					$select.attr("disabled", "disabled");
@@ -53,11 +52,11 @@ var AD = function() {
 			}
 
 			// 取消
-			$('#AD .form-actions button[title="cancel"]').live(
+			$('#cancel').live(
 					'click',
 					function() {
 						var key = $('#AD_key').val();
-						var items = jsondata[key].items;
+						var items = jsondata[key];
 						fillForm(items);
 
 						$("#AD form").find("select").attr("disabled",
@@ -69,60 +68,46 @@ var AD = function() {
 					});
 
 			// 编辑
-			$('#AD .actions a[title="edit"]').live('click', function() {
+			$('#edit').live('click', function() {
 				$("#AD form").find("select").removeAttr("disabled");
 				$("#AD form").find("input").removeAttr("disabled");
 				$("#AD_key").attr("disabled", "disabled");
 				$('#AD .form-actions').show();
 			});
 
-			// 删除
-			$('#AD .actions a[title="del"]').live(
-					'click',
-					function() {
-						var key = $('#AD_key').val();
-						$.post('../SetupConfServlet', {
-							"operation" : "delete",
-							"key" : key
-						}, function(data) {
-							alert(data.msg);
-							if (data.success == true) {
-								// $('#AD_key option[text="'+key+'"]').remove();
-								$('#AD_key')
-										.remove(
-												$('#AD_key option[text="' + key
-														+ '"]'));
-								console.log($(
-										'#AD_key option[text="' + key + '"]')
-										.text());
-								delete jsondata.key;
-								$("#AD_key").first().attr("selected",
-										"selected");
-								console.log(jsondata);
-							}
-						}, "json");
-					});
-
 			// 保存
-			$('#AD .form-actions button[title="ok"]').live(
+			$('#save').live(
 					'click',
 					function() {
-
 						var data = $('#AD form').serialize();
 						var key = $('#AD_key').val();
 						var array = data.split('&');
 						$.post('../SetupConfServlet?operation=update&key='
 								+ key, data, function(data) {
-							var items = jsondata[key].items;
+							alert(data.msg);
 							if (data.success == true) {
-								var index = 0;
-								for ( var i in items) {
-									var item = items[i];
-									item.value = array[i].split('=')[1];
-									index++;
-								}
+								// 请求数据
+								$.post("../SetupConfServlet", {
+									"operation" : "query",
+									"key" : "AD",
+									"type" : "multi"
+								}, function(data) {
+									$('#AD_key').empty();
+									jsondata = data;
+									var i = 0;
+									for ( var config in data) {
+										$('#AD_key').append(
+												$('<option>').text(config));
+										if(key==config){
+											$("#AD_key").find("option:contains('"+key+"')").attr("selected",
+											"selected");
+											fillForm(jsondata[config]);
+										}
+									}
+
+								}, "json");
 							} else {
-								fillForm(items);
+								fillForm(jsondata[key]);
 							}
 							$("#AD form").find("select").attr("disabled",
 									"disabled");
@@ -131,7 +116,6 @@ var AD = function() {
 							$("#AD_key").removeAttr("disabled");
 							$('#AD .form-actions').hide();
 
-							alert(data.msg);
 						}, "json");
 					});
 
